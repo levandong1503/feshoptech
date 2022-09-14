e138fc6288894b64b7669b15c853357886007494
DN12h <dai.23102002@gmial.com>
2022-09-15 00:45:26 +0700

    add config format

--- src/App.js
+++ src/App.js
@@ -3,47 +3,61 @@ import './App.css';
 import ProductDetail from './ProductDetail/ProductDetail';
 import Product from './Product/Product';
 function App() {
-  
-  return (
-    <div className='root'>
-      <header>
-        <h1 className="logo">
-            DIENTUTHOTIEN
-        </h1>
-        <form className="search-product">
-            <input type="text" placeholder="Hôm nay bạn muốn mua gì ?"/>
-            <button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
-        </form>
-        <a className="kt-dh" href="#">
-            <i className="fa-regular fa-car-side"></i>
-            <span>
-                Kiểm tra đơn hàng
-            </span>
-        </a>
-        <a className="card">
-            <i className="fa-light fa-cart-shopping"></i>
-            <span className="angle"></span>
-            <span className="number-product">0</span>
-        </a>
+    return (
         <div className="root">
+            <header>
+                <h1 className="logo">DIENTUTHOTIEN</h1>
+                <form className="search-product">
+                    <input type="text" placeholder="Hôm nay bạn muốn mua gì ?" />
+                    <button type="submit">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+                <a className="kt-dh" href="#">
+                    <i className="fa-regular fa-car-side"></i>
+                    <span>Kiểm tra đơn hàng</span>
+                </a>
+                <a className="card">
+                    <i className="fa-light fa-cart-shopping"></i>
+                    <span className="angle"></span>
+                    <span className="number-product">0</span>
+                </a>
+                <div className="root"></div>
+            </header>
+            <nav>
+                <a className="nav-item" href="#">
+                    ĐIỆN THOẠI
+                </a>
+                <a className="nav-item" href="#">
+                    ĐỒNG HỒ
+                </a>
+                <a className="nav-item" href="#">
+                    LAPTOP
+                </a>
+                <a className="nav-item" href="#">
+                    TAI NGHE
+                </a>
+                <a className="nav-item" href="#">
+                    TABLET
+                </a>
+                <a className="nav-item" href="#">
+                    ĐỒ CHƠI CÔNG NGHỆ
+                </a>
+            </nav>
+            <div className="container">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
         </div>
-    </header>
-    <nav>
-        <a className="nav-item" href="#" >ĐIỆN THOẠI</a>
-        <a className="nav-item" href="#">ĐỒNG HỒ</a>
-        <a className="nav-item" href="#">LAPTOP</a>
-        <a className="nav-item" href="#">TAI NGHE</a>
-        <a className="nav-item" href="#">TABLET</a>
-        <a className="nav-item" href="#">ĐỒ CHƠI CÔNG NGHỆ</a>
-    </nav>
-    <div className='container'>
-      
-        <Product /><Product /><Product /><Product /><Product />
-        <Product /><Product /><Product /><Product /><Product />
-    </div>
-  </div>
-    
-  );
+    );
 }
 
 export default App;
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -8,7 +8,8 @@ function Product(){
             <img className={style.imgProduct} src="https://cdn.hoanghamobile.com/i/productlist/ts/Uploads/2022/01/27/thumb.jpg" />
             <div className={style.btCP}>
                 <div className={style.infor}>Samsung Galaxy S22 - 8GB/256GB - Chính hãng</div>
-                <div className={style.price}>3,890,000 d</div>
+                <div className={style.
+                    price}>3,890,000 d</div>
             </div>
         </a>
 
